5c1a34f65d9c832a8e769591f3019537f4f2bda7
pantherg221 <njo.goodwin@gmail.com>
2017-09-25 20:54:31 -0700

    Adding Common src modules

---
+++ src/Common/CommonVersionInfo.cs
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.1.0.*")]

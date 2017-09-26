b8a3897b49ac1300002db419527d049c3f0efeb9
pantherg221 <njo.goodwin@gmail.com>
2017-09-25 22:13:43 -0700

    Initial Upload

---
+++ src/NzbDrone.Update/UpdateContainerBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using NzbDrone.Common.Composition;
+using NzbDrone.Common.EnvironmentInfo;
+using NzbDrone.Common.Http.Dispatchers;
+
+namespace NzbDrone.Update
+{
+    public class UpdateContainerBuilder : ContainerBuilderBase
+    {
+        private UpdateContainerBuilder(IStartupContext startupContext, string[] assemblies)
+            : base(startupContext, assemblies)
+        {
+            Container.Register<IHttpDispatcher, FallbackHttpDispatcher>();
+        }
+
+        public static IContainer Build(IStartupContext startupContext)
+        {
+            var assemblies = new List<string>
+                             {
+                                 "Radarr.Update",
+                                 "NzbDrone.Common"
+                             };
+
+            if (OsInfo.IsWindows)
+            {
+                assemblies.Add("NzbDrone.Windows");
+            }
+
+            else
+            {
+                assemblies.Add("NzbDrone.Mono");
+            }
+
+            return new UpdateContainerBuilder(startupContext, assemblies.ToArray()).Container;
+        }
+    }
+}
+ 
---
+++ src/NzbDrone.Windows/EnvironmentInfo/DotNetRuntimeProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NLog;
+using NzbDrone.Common.EnvironmentInfo;
+
+namespace NzbDrone.Windows.EnvironmentInfo
+{
+    public class DotNetRuntimeProvider : RuntimeInfoBase
+    {
+        public DotNetRuntimeProvider(Common.IServiceProvider serviceProvider, Logger logger)
+            : base(serviceProvider, logger)
+        {
+        }
+
+        public override string RuntimeVersion => Environment.Version.ToString();
+    }
+}
